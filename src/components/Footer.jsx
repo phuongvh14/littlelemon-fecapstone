6f5be60d029a8f03162c00d09ec87ca342108ab7
Phuong Vu <hhanhphuongg14@gmail.com>
2023-03-02 16:35:58 +0700

    Finalize footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,16 +27,30 @@ export default function Footer() {
                     <div className="address">
                         <div className="section-title">
                             <i class="fa-solid fa-map-location-dot fa-lg"></i>
-                            <h1>Address</h1>
+                            <h1>Addresses</h1>
                         </div>
                         <div className="section-content">
-                            <p>Addresses here</p>
+                            <p>420 W Belmont Ave,</p>
+                            <p>Chicago, IL 60657</p>
+                            <br></br>
+                            <p>88 Russell St,</p>
+                            <p>Lewiston, ME 04240</p>
                         </div>
                     </div>
                     <div className="connect">
                         <div className="section-title">
-                        <i class="fa-sharp fa-solid fa-address-book fa-lg"></i>
+                            <i class="fa-sharp fa-solid fa-address-book fa-lg"></i>
                             <h1>Connect</h1>
+                        </div>
+                        <div className="section-content">
+                            <p>Tel: (773)-961-8196</p>
+                            <p>Email: <a href='mailto:info@littlelemon.com'>info@littlelemon.com</a></p>
+                        </div>
+                        <div className="social-media">
+                            <i class="fa-brands fa-square-facebook fa-2xl"></i>
+                            <i class="fa-brands fa-instagram fa-2xl"></i>
+                            <i class="fa-brands fa-tiktok fa-2xl"></i>
+                            <i class="fa-brands fa-twitter fa-2xl"></i>
                         </div>
                     </div>
                 </div>
